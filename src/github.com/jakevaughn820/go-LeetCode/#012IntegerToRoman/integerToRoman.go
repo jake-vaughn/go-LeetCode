993c1dc8e07d69fbafd331e47e90312b2e48a641
jakevaughn820 <jakevaughn820@gmail.com>
2022-07-18 18:33:49 -0500

    Solved #012IntegerToRoman

--- src/github.com/jakevaughn820/go-LeetCode/#012IntegerToRoman/integerToRoman.go
+++ src/github.com/jakevaughn820/go-LeetCode/#012IntegerToRoman/integerToRoman.go
@@ -2,11 +2,51 @@ package main
 
 import "fmt"
 
-func intToRoman(num int) string {
+func stringNTimes(s string, n int) string {
+	ret := ""
+	for i := 1; i <= n; i++ {
+		ret += s
+	}
+	return ret
+}
 
-	return ""
+func intToRoman(num int) string {
+	ret := ""
+	one, five, ten := "", "", ""
+
+	for i := 1; num > 0; i++ {
+		switch i {
+		case 1:
+			one = "I"
+			five = "V"
+			ten = "X"
+		case 2:
+			one = "X"
+			five = "L"
+			ten = "C"
+		case 3:
+			one = "C"
+			five = "D"
+			ten = "M"
+		case 4:
+			one = "M"
+		}
+		n := num % 10
+		switch {
+		case n < 4:
+			ret = stringNTimes(one, n) + ret
+		case n == 4:
+			ret = one + five + ret
+		case n == 9:
+			ret = one + ten + ret
+		case n >= 5:
+			ret = five + stringNTimes(one, n-5) + ret
+		}
+		num /= 10
+	}
+	return ret
 }
 
 func main() {
-	fmt.Println(intToRoman(10))
+	fmt.Println(intToRoman(28))
 }
