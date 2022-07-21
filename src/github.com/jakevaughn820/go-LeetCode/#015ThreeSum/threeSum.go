6ceedf6bc93b5102ed1812271e1a9e4b55905821
jakevaughn820 <jakevaughn820@gmail.com>
2022-07-20 21:33:21 -0500

    Updated #015 ThreeSum and added comments

--- src/github.com/jakevaughn820/go-LeetCode/#015ThreeSum/threeSum.go
+++ src/github.com/jakevaughn820/go-LeetCode/#015ThreeSum/threeSum.go
@@ -2,14 +2,49 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
+/*
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+
+Notice that the solution set must not contain duplicate triplets.
+*/
 func threeSum(nums []int) [][]int {
 	ret := [][]int{}
 
+	//Sorts nums smallest to biggest
+	sort.Slice(nums, func(i, j int) bool {
+		return nums[i] < nums[j]
+	})
+
+	//Ranges over nums to find triplets
+	for i, a := range nums {
+		//if a is = to last num skip it
+		if i > 0 && a == nums[i-1] {
+			continue
+		}
+		l, r := i+1, len(nums)-1
+		for l < r {
+			if nums[l] == nums[l-1] && l-1 != i {
+				l++
+				continue
+			}
+			bcSum := nums[l] + nums[r]
+			if bcSum == -a {
+				ret = append(ret, []int{a, nums[l], nums[r]})
+				l++
+			} else if bcSum < -a {
+				l++
+			} else {
+				r--
+			}
+		}
+	}
 	return ret
 }
 
 func main() {
-	fmt.Println(threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Println(threeSum([]int{-1, -14, 0, 1, 0, 0, 0, 2, -1, -4, -14, 28}))
 }
