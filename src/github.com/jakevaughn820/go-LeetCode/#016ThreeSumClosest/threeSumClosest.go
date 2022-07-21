f3a2b3d0599b2b91176801d8aeab1efc53e25147
jakevaughn820 <jakevaughn820@gmail.com>
2022-07-20 21:38:19 -0500

    Deleted #167 and started #016

---
+++ src/github.com/jakevaughn820/go-LeetCode/#016ThreeSumClosest/threeSumClosest.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func threeSumClosest(nums []int, target int) int {
+	ret := 0
+
+	return ret
+}
+
+func main() {
+	fmt.Println(threeSumClosest([]int{-1, 2, 1, -4}, 1))
+}
