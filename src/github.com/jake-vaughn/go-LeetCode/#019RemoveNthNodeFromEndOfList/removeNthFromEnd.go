23a292b6323126d89c4874c1a27d0ba3d7215991
jakevaughn820 <jakevaughn820@gmail.com>
2022-08-17 14:18:27 -0500

    changed username to jake-vaughn

---
+++ src/github.com/jake-vaughn/go-LeetCode/#019RemoveNthNodeFromEndOfList/removeNthFromEnd.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+//Definition for singly-linked list.
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+
+	return nil
+}
+
+func main() {
+	fmt.Println(removeNthFromEnd(nil, 4))
+}
